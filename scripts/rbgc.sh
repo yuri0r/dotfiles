3cf28603608c817aad188ae632208a458388815d
macktrinz@yahoo.de <macktrinz@yahoo.de>
2019-09-18 12:58:04 +0200

    improved background script to apply random images to evry screen

--- scripts/rbgc.sh
+++ scripts/rbgc.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 #script assumes that nitrogen is setup
 
-nitrogen --set-auto --random --save 
+#get number of screens
+var=` xrandr | grep ' connected' | wc -l`
+
+#set random background for each screen
+for ((i=0; i<$var; i++)) 
+do
+	echo $i
+	nitrogen --set-scaled --head=$i --save --random $HOME/wallpaper 
+done
